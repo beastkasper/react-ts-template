cdc620fc787a96b6e218554a9abd37804383d1e0
AminjonM <aminmirzoev123@gmail.com>
2025-01-30 13:20:36 +0500

    add patch version 1.0.2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppRouter } from "./routes";
 
 const App: React.FC = () => {
-    console.log("check release version 1.0.1");
+    console.log("check release version 1.0.2");
     return <AppRouter />;
 };
 
